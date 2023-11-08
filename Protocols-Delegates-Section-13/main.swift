22bd3ec51e0c63588e061e1f0e539305d6211496
Abdurahman AYDIN <hariysu@gmail.com>
2023-11-08 10:09:05 +0300

    Shows delegate design pattern

--- Protocols-Delegates-Section-13/main.swift
+++ Protocols-Delegates-Section-13/main.swift
@@ -5,7 +5,64 @@
 //  Created by Abdurahman on 6.11.2023.
 //
 
-import Foundation
 
-print("Hello, World!")
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+//UITextField
+class EmergencyCallHandler{
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation(){
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency(){
+        delegate?.performCPR()
+    }
+}
+
+//It can be delegate
+struct Paramedic: AdvancedLifeSupport{
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+//It can be delegate
+class Doctor: AdvancedLifeSupport{
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second.")
+    }
+    func useStethescope(){
+        print("Listening for heart sounds")
+    }
+}
+
+//It can be delegate
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeeGees")
+    }
+    func useElectricDrill(){
+        print("Whirr...")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+//let pete = Paramedic(handler: emilio)
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
+
 
---
+++ README.md
@@ -0,0 +1,8 @@
+Protocols-Delegates-Section-13
+
+You can use below figure to grasp delegates and protocols simply.
+
+![ProtocolsDemo Section 13](Documentation/ss-1.png)
+
+
+This repo is not effective to understand delegates and protocols.
